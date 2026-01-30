fafe02a07108dc38983d25800a73559047933ca6
seunggi <hsg77689469@gmail.com>
2026-01-30 18:33:25 +0900

    트랜잭션 전파 활용4 - 전파 커밋

--- src/main/java/hello/springtx/propagation/MemberService.java
+++ src/main/java/hello/springtx/propagation/MemberService.java
@@ -3,12 +3,16 @@ package hello.springtx.propagation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
     private final LogRepository logRepository;
+
+    @Transactional //추가
     public void joinV1(String username) {
         Member member = new Member(username);
         Log logMessage = new Log(username);
